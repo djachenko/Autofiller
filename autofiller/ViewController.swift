cbdc6a5cf277d9a98dbaed588004a87ac522545b
Igor Djachenko <i.s.djachenko@gmail.com>
2019-11-25 23:30:06 +0700

    Initial Commit

---
+++ autofiller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  autofiller
+//
+//  Created by justin on 25/11/2019.
+//  Copyright © 2019 djachenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
